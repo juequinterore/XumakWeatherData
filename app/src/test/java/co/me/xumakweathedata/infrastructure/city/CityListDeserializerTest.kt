4a64e7adb81afc0f91372b8adb45a183f037d11a
Juan E Quintero R <juan@57blocks.com>
2021-09-10 18:01:05 -0500

    feat: add CityListDeserializer.kt

---
+++ app/src/test/java/co/me/xumakweathedata/infrastructure/city/CityListDeserializerTest.kt
@@ -0,0 +1,89 @@
+package co.me.xumakweathedata.infrastructure.city
+
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.reflect.TypeToken
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import java.io.File
+import java.lang.reflect.Type
+import java.nio.file.Paths
+
+class CityListDeserializerTest {
+
+    @Test
+    fun `should convert cities response into List of cities`() {
+        //Arrange
+        val cityListDeserializer = CityListDeserializer()
+        val path = Paths.get("").toAbsolutePath().toString()
+        val multiplesCities =
+            File("$path/src/test/java/co/me/xumakweathedata/infrastructure/city/fixtures/multipleCities.json").readText()
+
+        //Act
+        val type: Type =
+            object : TypeToken<List<@kotlin.jvm.JvmSuppressWildcards CityDto>>() {}.type
+        val deserializedCities =
+            cityListDeserializer.deserialize(
+                Gson().fromJson(
+                    multiplesCities,
+                    JsonElement::class.java
+                ), type, null
+            )
+
+        //Assert
+        val expectedCities = listOf(
+            CityDto(
+                geonameid = 4053050,
+                name = "Calera",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_austin.png",
+                isTopCity = false,
+                weather = emptyMap()
+            ),
+
+            CityDto(
+                geonameid = 4054852,
+                name = "Chelsea",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_dallas.png",
+                isTopCity = false,
+                weather = emptyMap()
+            ),
+
+            CityDto(
+                geonameid = 4049979,
+                name = "Birmingham",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_austin.png",
+                isTopCity = false,
+                weather = emptyMap()
+            ),
+
+            CityDto(
+                geonameid = 4055045,
+                name = "Childersburg",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_sanfranisco.png",
+                isTopCity = false,
+                weather = emptyMap()
+            ),
+
+            CityDto(
+                geonameid = 4048888,
+                name = "Red Chute",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_dallas.png",
+                isTopCity = false,
+                weather = emptyMap()
+            ),
+
+            CityDto(
+                geonameid = 4054996,
+                name = "Chickasaw",
+                imageUrl = "https://all-the-weather-resources.s3.amazonaws.com/Images/Android_City_Images/xhdpi/img_austin.png",
+                isTopCity = false,
+                weather = emptyMap()
+            )
+        )
+        println("Deserialized: $deserializedCities")
+
+
+        assertEquals(deserializedCities, expectedCities)
+    }
+
+}
