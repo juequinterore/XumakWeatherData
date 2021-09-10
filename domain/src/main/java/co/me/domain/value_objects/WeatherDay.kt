a1d17bc20f5df4f60c8d5ce5a5a91d4da859b9c2
Juan E Quintero R <juan@57blocks.com>
2021-09-09 22:33:40 -0500

    feat: create WeatherDay value object

---
+++ domain/src/main/java/co/me/domain/value_objects/WeatherDay.kt
@@ -0,0 +1,9 @@
+package co.me.domain.value_objects
+
+data class WeatherDay(
+    val dayOfTheWeek: WeekDay,
+    val low: Int,
+    val high: Int,
+    val weatherType: WeatherType,
+    val hourlyWeather: List<HourlyWeather>
+)
