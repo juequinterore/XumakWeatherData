90598f31a876613059fc8bbb96bc94eea88c71ed
Juan E Quintero R <juan@57blocks.com>
2021-09-09 22:33:04 -0500

    feat: create XUrl value object

---
+++ domain/src/main/java/co/me/domain/value_objects/XUrl.kt
@@ -0,0 +1,13 @@
+package co.me.domain.value_objects
+
+val pattern =
+    "[(http(s)?):\\/\\/(www\\.)?a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)".toRegex()
+
+@JvmInline
+value class XUrl(val value: String) {
+    init {
+        require(pattern.matches(value)) {
+            "$value is not a valid url"
+        }
+    }
+}
