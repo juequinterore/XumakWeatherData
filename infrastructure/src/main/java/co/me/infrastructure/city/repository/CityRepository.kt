e231e6ac2149b6522d0db1bc516547e872a6c9b8
Juan E Quintero R <juan@57blocks.com>
2021-09-11 01:49:02 -0500

    feat: add support for getAllCities in CityRepository.kt

--- infrastructure/src/main/java/co/me/infrastructure/city/repository/CityRepository.kt
+++ infrastructure/src/main/java/co/me/infrastructure/city/repository/CityRepository.kt
@@ -2,14 +2,18 @@ package co.me.infrastructure.city.repository
 
 import co.me.domain.city.ICityRepository
 import co.me.domain.entities.City
+import co.me.infrastructure.city.repository.data_sources.ICityLocalDataSource
 import co.me.infrastructure.city.repository.data_sources.ICityRemoteDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class CityRepository(private val remoteRemoteDataSource: ICityRemoteDataSource) : ICityRepository {
-    override fun getAllCities(): Flow<List<City>> {
-        TODO("Not yet implemented")
-    }
+class CityRepository(
+    private val remoteCityDataSource: ICityRemoteDataSource,
+    private val localCityDataSource: ICityLocalDataSource
+) : ICityRepository {
+    override fun getAllCities(): Flow<List<City>> =
+        localCityDataSource.getAllCities().map { allCityDts -> allCityDts.map { it.toDomain() } }
 
     override suspend fun searchCityByName(name: String): City? =
-        remoteRemoteDataSource.searchCityByName(name)?.toDomain()
+        remoteCityDataSource.searchCityByName(name)?.toDomain()
 }
--- infrastructure/src/test/java/co/me/infrastructure/city/repository/CityRepositoryTest.kt
+++ infrastructure/src/test/java/co/me/infrastructure/city/repository/CityRepositoryTest.kt
@@ -2,9 +2,14 @@ package co.me.infrastructure.city.repository
 
 import co.me.infrastructure.city.dtos.CityDto
 import co.me.infrastructure.city.dtos.WeatherDayDto
+import co.me.infrastructure.city.repository.data_sources.ICityLocalDataSource
 import co.me.infrastructure.city.repository.data_sources.ICityRemoteDataSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class FakeCityRemoteDataSource(private val searchCity: CityDto? = null) : ICityRemoteDataSource {
@@ -26,13 +31,46 @@ class FakeCityRemoteDataSource(private val searchCity: CityDto? = null) : ICityR
 
 }
 
+class FakeCityLocalDataSource(private val allCities: List<CityDto> = emptyList()) :
+    ICityLocalDataSource {
+
+    private var _isGetAllCitiesCalled = false
+
+    val isGetAllCitiesCalled
+        get() = _isGetAllCitiesCalled
+
+    override fun getAllCities(): Flow<List<CityDto>> {
+        _isGetAllCitiesCalled = true
+        return flow { emit(allCities) }
+    }
+
+}
+
 class CityRepositoryTest {
+
+    private fun createCityRepository(
+        searchCity: CityDto? = null,
+        allCities: List<CityDto> = emptyList()
+    ): CityRepository {
+
+        val mockCityRemoteDataSource = FakeCityRemoteDataSource(searchCity = searchCity)
+        val fakeCityLocalDataSource = FakeCityLocalDataSource(allCities = allCities)
+        return CityRepository(
+            remoteCityDataSource = mockCityRemoteDataSource,
+            localCityDataSource = fakeCityLocalDataSource
+        )
+
+    }
 
     @Test
     fun `should call remoteDataSource with received name`() = runBlocking {
         //Arrange
         val mockCityRemoteDataSource = FakeCityRemoteDataSource()
-        val cityRepository = CityRepository(remoteRemoteDataSource = mockCityRemoteDataSource)
+        val fakeCityLocalDataSource = FakeCityLocalDataSource()
+        val cityRepository = CityRepository(
+            remoteCityDataSource = mockCityRemoteDataSource,
+            localCityDataSource = fakeCityLocalDataSource
+        )
 
         //Act
         cityRepository.searchCityByName("Medellín")
@@ -50,14 +88,55 @@ class CityRepositoryTest {
             imageUrl = "https://myImage.co/image.jpg",
             weather = mapOf()
         )
-        val mockCityRemoteDataSource = FakeCityRemoteDataSource(fakeCityDto)
-        val cityRepository = CityRepository(remoteRemoteDataSource = mockCityRemoteDataSource)
+        val cityRepository = createCityRepository(searchCity = fakeCityDto)
 
         //Act
         val city = cityRepository.searchCityByName("Medellín")
 
         //Assert
         assertEquals(city, fakeCityDto.toDomain())
+    }
+
+    @Test
+    fun `should call localDataSource to get all cities`() = runBlocking {
+        //Arrange
+        val fakeCityRemoteDataSource = FakeCityRemoteDataSource()
+        val mockCityLocalDataSource = FakeCityLocalDataSource()
+        val cityRepository = CityRepository(
+            remoteCityDataSource = fakeCityRemoteDataSource,
+            localCityDataSource = mockCityLocalDataSource
+        )
+
+        //Act
+        cityRepository.getAllCities()
+
+        //Assert
+        assertTrue(mockCityLocalDataSource.isGetAllCitiesCalled)
+
+    }
+
+    @Test
+    fun `should return localDataSource CityDtos converted into Cities`() = runBlocking {
+        //Arrange
+        val fakeCityDto = CityDto(
+            geonameid = 20001201,
+            name = "Medellín",
+            imageUrl = "https://myImage.co/image.jpg",
+            weather = mapOf()
+        )
+        val fakeCityDto2 = CityDto(
+            geonameid = 2111221,
+            name = "Bogotá",
+            imageUrl = "https://myImage.co/image2.jpg",
+            weather = mapOf()
+        )
+        val cityRepository = createCityRepository(allCities = listOf(fakeCityDto, fakeCityDto2))
+
+        //Act
+        val cities = cityRepository.getAllCities().first()
+
+        //Assert
+        assertEquals(cities, listOf(fakeCityDto.toDomain(), fakeCityDto2.toDomain()))
     }
 
 }
