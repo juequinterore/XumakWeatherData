c2975d816768fd417079e7f2fa416f9207410ac7
Juan E Quintero R <juan@57blocks.com>
2021-09-11 16:24:50 -0500

    feat: add IInitialCitiesRequest.kt application use case

---
+++ app/src/main/java/co/me/xumakweathedata/main/use_cases/IInitialCitiesRequest.kt
@@ -0,0 +1,34 @@
+package co.me.xumakweathedata.main.use_cases
+
+import co.me.domain.city.ICityRepository
+import co.me.domain.city.use_cases.IGetAllCities
+import co.me.domain.city.use_cases.ISearchCityByName
+import co.me.domain.city.use_cases.SearchCityByNameCommand
+import co.me.domain.use_cases.ICommandUseCase
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onEach
+
+interface IInitialCitiesRequest : ICommandUseCase<Unit>
+
+class InitialCitiesCitiesRequest(
+    private val getAllCities: IGetAllCities,
+    private val searchCityByName: ISearchCityByName,
+    private val citiesRepository: ICityRepository
+) :
+    IInitialCitiesRequest {
+    override suspend fun invoke(command: Unit) {
+        getAllCities(Unit).onEach {
+            if (it.isEmpty()) {
+                requestDefaultCity()
+            }
+        }.collect()
+    }
+
+    private suspend fun requestDefaultCity() {
+        val city = searchCityByName(SearchCityByNameCommand("Trinity"))
+        city?.let {
+            citiesRepository.insert(it)
+        }
+    }
+
+}
