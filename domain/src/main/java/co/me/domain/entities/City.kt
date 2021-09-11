a39d98c9c2f010a744d77d83feb558b983809216
Juan E Quintero R <juan@57blocks.com>
2021-09-11 16:21:49 -0500

    feat: set City.kt most fields as immutable

--- domain/src/main/java/co/me/domain/entities/City.kt
+++ domain/src/main/java/co/me/domain/entities/City.kt
@@ -6,9 +6,9 @@ import co.me.domain.value_objects.XUrl
 
 data class City(
     val id: Int,
-    val name: String,
-    val imageUrl: XUrl,
-    val weather: Map<WeekDay, WeatherDay>
+    var name: String,
+    var imageUrl: XUrl,
+    var weather: Map<WeekDay, WeatherDay>
 ) {
     fun getWeatherForDay(weekDay: WeekDay): WeatherDay? = weather[weekDay]
 }
