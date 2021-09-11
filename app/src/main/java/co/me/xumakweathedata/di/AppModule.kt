188268b4c48a4bc3f19d041ae291ee6d2824628a
Juan E Quintero R <juan@57blocks.com>
2021-09-11 16:26:02 -0500

    feat: add MainViewModel to AppModule.kt

--- app/src/main/java/co/me/xumakweathedata/di/AppModule.kt
+++ app/src/main/java/co/me/xumakweathedata/di/AppModule.kt
@@ -2,6 +2,10 @@ package co.me.xumakweathedata.di
 
 import co.me.di.APIURLQualifier
 import co.me.xumakweathedata.BuildConfig
+import co.me.xumakweathedata.MainViewModel
+import co.me.xumakweathedata.main.use_cases.IInitialCitiesRequest
+import co.me.xumakweathedata.main.use_cases.InitialCitiesCitiesRequest
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
@@ -10,4 +14,13 @@ val appModule = module {
         BuildConfig.API_URL
     }
 
+    viewModel { MainViewModel(getAllCities = get(), initialCitiesRequest = get()) }
+
+    single<IInitialCitiesRequest> {
+        InitialCitiesCitiesRequest(
+            getAllCities = get(),
+            citiesRepository = get(),
+            searchCityByName = get()
+        )
+    }
 }
