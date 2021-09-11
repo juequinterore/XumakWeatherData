336dbc26b4daa052b147e6316e767677c280d6eb
Juan E Quintero R <juan@57blocks.com>
2021-09-10 22:31:37 -0500

    feat: add toDomain in CityDto.kt

--- app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/CityDto.kt
+++ app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/CityDto.kt
@@ -1,11 +1,27 @@
 package co.me.xumakweathedata.infrastructure.city.dtos
 
+import co.me.domain.entities.City
+import co.me.domain.value_objects.WeekDay
+import co.me.domain.value_objects.XUrl
+
 data class CityDto(
     val geonameid: Int,
     val name: String,
     val imageUrl: String,
     val isTopCity: Boolean,
     val weather: Map<Int, WeatherDayDto>
-)
+) {
+
+    fun toDomain(): City = City(
+        id = geonameid,
+        name = name,
+        imageUrl = XUrl(imageUrl),
+        isTopCity = false,
+        weather = weather.entries.fold(mutableMapOf()) { acc, entry ->
+            acc[WeekDay(entry.key)] = entry.value.toDomain()
+            acc
+        })
+
+}
 
 
