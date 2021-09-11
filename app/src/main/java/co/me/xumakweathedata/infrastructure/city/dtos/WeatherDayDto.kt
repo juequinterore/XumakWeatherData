2c8ea945f25a97bec5b370a24f84b419f4d63dea
Juan E Quintero R <juan@57blocks.com>
2021-09-10 22:31:20 -0500

    feat: add toDomain in WeatherDayDto.kt

--- app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/WeatherDayDto.kt
+++ app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/WeatherDayDto.kt
@@ -1,5 +1,10 @@
 package co.me.xumakweathedata.infrastructure.city.dtos
 
+import co.me.domain.value_objects.DayHour
+import co.me.domain.value_objects.WeatherDay
+import co.me.domain.value_objects.WeatherType
+import co.me.domain.value_objects.WeekDay
+
 data class WeatherDayDto(
     val dayOfTheWeek: Int,
     val weatherType: String,
@@ -7,5 +12,16 @@ data class WeatherDayDto(
     val high: Int,
     val hourlyWeather: Map<Int, HourlyWeatherDto>
 ) {
+    fun toDomain(): WeatherDay = WeatherDay(dayOfTheWeek = WeekDay(dayOfTheWeek),
+        weatherType = WeatherType.fromString(weatherType),
+        high = high,
+        low = low,
+        hourlyWeather = hourlyWeather.entries.fold(
+            mutableMapOf()
+        ) { acc, entry ->
+            acc[DayHour(entry.key)] = entry.value.toDomain()
+            acc
+        })
+
     companion object
 }
