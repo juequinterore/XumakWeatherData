1775420f0a46dda96301bcc656ef8c4f2df61b8a
Juan E Quintero R <juan@57blocks.com>
2021-09-10 22:31:04 -0500

    feat: add toDomain in HourlyWeatherDto.kt

--- app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/HourlyWeatherDto.kt
+++ app/src/main/java/co/me/xumakweathedata/infrastructure/city/dtos/HourlyWeatherDto.kt
@@ -1,12 +1,26 @@
 package co.me.xumakweathedata.infrastructure.city.dtos
 
+import co.me.domain.value_objects.DayHour
+import co.me.domain.value_objects.HourlyWeather
+import co.me.domain.value_objects.Probability
+import co.me.domain.value_objects.WeatherType
+
 data class HourlyWeatherDto(
     val rainChance: Double,
     val hour: Int,
     val humidity: Double,
     val weatherType: String,
-    val windSpeed: Int,
+    val windSpeed: Double,
     val temperature: Int
 ) {
+    fun toDomain(): HourlyWeather = HourlyWeather(
+        rainChance = Probability(rainChance),
+        hour = DayHour(hour),
+        humidity = Probability(humidity),
+        weatherType = WeatherType.fromString(weatherType),
+        windSpeed = windSpeed,
+        temperature = temperature
+    )
+
     companion object
 }
