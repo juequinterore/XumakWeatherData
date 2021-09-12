0aa546a49eb3c30617a50d0afab64d63668a42b3
Juan E Quintero R <juan@57blocks.com>
2021-09-12 13:17:29 -0500

    feat: remove unused import in MainActivity.kt

--- app/src/main/java/co/me/xumakweathedata/MainActivity.kt
+++ app/src/main/java/co/me/xumakweathedata/MainActivity.kt
@@ -1,7 +1,6 @@
 package co.me.xumakweathedata
 
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
