1be90fcb17a6d3c8fcbb453346d012998aa763c7
Juan E Quintero R <juan@57blocks.com>
2021-09-13 04:15:18 -0500

    feat: add @FlowPreview annotation to XumakWeatherApplication.kt

--- app/src/main/java/co/me/xumakweathedata/XumakWeatherApplication.kt
+++ app/src/main/java/co/me/xumakweathedata/XumakWeatherApplication.kt
@@ -4,11 +4,13 @@ import android.app.Application
 import co.me.di.domainModule
 import co.me.di.infrastructureModule
 import co.me.xumakweathedata.di.appModule
+import kotlinx.coroutines.FlowPreview
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class XumakWeatherApplication : Application() {
 
+    @FlowPreview
     override fun onCreate() {
         super.onCreate()
 
